1915d14b776bbd19d1de88cc045a2d74336e6841
Henrique Yuji Yamada <henriqueyamada01@gmail.com>
2026-05-05 10:17:37 -0300

    learning php strings and operations

--- screen-match.php
+++ screen-match.php
@@ -1,18 +1,28 @@
 <?php
-echo "Bem-vindo(a) ao Screen Match!
-";
+echo "Bem-vindo(a) ao Screen Match! \n";
 
 // Aprendendo a usar variáveis do tipo int, float e boolean
 
 $nomeFilme = "Top Gun: Maverick";
-$anoLancamento = 2022;
+$anoLancamento = $argv[1] ?? 2022; // Usando o operador de coalescência nula para definir um valor padrão
 $somaAvaliacoes = 9 + 6 + 8 + 7.5 + 5;
 $notaFilme = $somaAvaliacoes / 5;
 
 $planoPrime = true;
 $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 
-echo $notaFilme;
-echo $incluidoNoPlano;
-echo $incluidoNoPlano ? 'True' : 'False';
+//echo $notaFilme;
+//echo $incluidoNoPlano;
+//echo $incluidoNoPlano ? 'True' : 'False';
 
+// Aprendendo a usar variáveis do tipo string: Concatenação e Interpolação
+
+//echo "Nome do filme: " . $nomeFilme . "\n";
+//echo "Nota do filme: $notaFilme \n";
+
+//echo 'Nome do filme: ' . $nomeFilme . '\n';
+//echo 'Nota do filme: $notaFilme \n';
+
+echo "Nome do filme: $nomeFilme \n";
+echo "Nota do filme: $notaFilme \n";
+echo "Ano de lançamento: $anoLancamento \n";
