366824a394805fdc7469aa604e07f11b9225a1b3
Henrique Yamada <henriqueyamada01@gmail.com>
2026-05-06 23:45:41 -0300

    Course 2 - List exercise 4 and updates

--- lista-exercicios/lista04.php
+++ lista-exercicios/lista04.php
@@ -14,7 +14,7 @@ while ($cont <= 100) {
 
 // 2 - Tabuada
 
-/*$cont = 1;
+$cont = 1;
 echo "Digite um número para a tabuada: \n> ";
 $num = (int) fgets(STDIN);
 
@@ -22,4 +22,28 @@ while ($cont <= 10) {
     $resultado = $num * $cont;
     echo $num . " x " . $cont . " = " . $resultado . "\n";
     $cont++;
-}*/
+}
+
+//3 - IMC
+
+echo "Qual a sua altura? \n> ";
+$altura = (float) fgets(STDIN);
+echo "Qual o seu peso? \n > ";
+$peso = (float) fgets(STDIN);
+$imc = $peso / $altura ** 2;
+
+if ($imc < 18.5) {
+    $resp = "Abaixo do peso normal";
+} elseif ($imc >= 18.5 && $imc < 25) {
+    $resp = "Peso normal";
+} elseif ($imc >= 25 && $imc < 30) {
+    $resp = "Excesso de peso";
+} elseif ($imc >= 30 && $imc < 35) {
+    $resp = "Obesidade classe I";
+} elseif ($imc >= 35 && $imc < 40) {
+    $resp = "Obesidade classe II";
+} else {
+    $resp = "Obesidade classe III";
+}
+
+echo $resp;
