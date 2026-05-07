5a314b525e645638335481f44fb5c4e7d359ce17
Henrique Yuji Yamada <henriqueyamada01@gmail.com>
2026-05-07 08:18:13 -0300

    List 5 - exercise 2 done

--- lista-exercicios/lista05.php
+++ lista-exercicios/lista05.php
@@ -28,3 +28,32 @@ $num2 = 7;
 calculadora($operacao, $num1, $num2);
 echo "\n";
 
+// 2- Crie uma função em PHP que calcule o IMC baseado na altura e peso passados por parâmetro.
+
+function calcImc(float $imc): string {
+    if ($imc < 18.5) {
+        $resp = "Abaixo do peso normal";
+    } elseif ($imc >= 18.5 && $imc < 25) {
+        $resp = "Peso normal";
+    } elseif ($imc >= 25 && $imc < 30) {
+        $resp = "Excesso de peso";
+    } elseif ($imc >= 30 && $imc < 35) {
+        $resp = "Obesidade classe I";
+    } elseif ($imc >= 35 && $imc < 40) {
+        $resp = "Obesidade classe II";
+    } else {
+        $resp = "Obesidade classe III";
+    }
+
+    return $resp;
+}
+
+echo "Qual a sua altura? \n> ";
+$altura = (float) fgets(STDIN);
+echo "Qual o seu peso? \n > ";
+$peso = (float) fgets(STDIN);
+$imc = $peso / $altura ** 2;
+
+$respImc = calcImc($imc);
+echo $respImc . "\n";
+
