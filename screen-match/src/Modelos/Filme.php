a309e1dd6d51f38da322fb1e96e03132cd5afe5c
Henrique Yuji Yamada <henriqueyamada01@gmail.com>
2026-05-08 09:41:08 -0300

    learning enums' method - PhP POO

--- screen-match/src/Modelos/Filme.php
+++ screen-match/src/Modelos/Filme.php
@@ -1,11 +1,16 @@
 <?php
 
 class Filme {
-    private string $nome = 'Nome padrão';
-    private int $anoLancamento = 2024;
-    private string $genero = 'ação';
     private float $media;
     private array $notas = []; //usado apenas para a própria clss
+
+    public function __construct(
+        public readonly string $nome, 
+        public readonly int $anoLancamento, 
+        public readonly Genero $genero
+        ) { //método construct: chamado automaticamente quando um objeto é instanciado (não pode ter um tipo de retorno)
+        $this->notas = []; // qualquer inicialização é colocado no construct
+    } //readonly - são publicas para leituras e privadas para mudança (um só vez)
 
     public function avalia(float $nota): void {
         $this->notas[] = $nota;
@@ -18,23 +23,30 @@ class Filme {
         return $somaNotas / $qntdNotas;
     }
 
-    /**
-     * Método getter, para podermos ler o ano de lançamento
-    */
+    /*
+    Método getter, para podermos ler o ano de lançamento
 
     public function anoLancamento(): int { // getter - recuperador (getAnoLancamento ())
         return $this->anoLancamento;
     }
 
-    /**
-     * Método setter, para que possamos alterar o ano de lançamento
-    */
+    Método setter, para que possamos alterar o ano de lançamento
 
     public function defineAnoLancamento(int $anoLancamento): void {
         $this->anoLancamento = $anoLancamento; // o $anoLancamento é definido dentro deste método, já que a propriedade é privada, que só pode ser alterada dentro dessa classe, e não exteriormente
     }
 
-    // para os nomes:
+    public function nome(): string {
+        return $this->nome;
+    }
+
+    public function genero(): string {
+        return $this->genero;
+    }
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+    para os nomes:
 
     public function nome(): string { //getter
         return $this->nome;
@@ -52,5 +64,5 @@ class Filme {
 
     public function defineGenero(string $genero): void { //setter - define uma novo valor 
         $this->genero = $genero;
-    }
+    }*/
 }
