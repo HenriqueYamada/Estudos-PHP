5162202b8fd3068e4a2c3f56ca8d144feae650b7
Henrique Yuji Yamada <henriqueyamada01@gmail.com>
2026-05-11 07:48:50 -0300

    learning MVC with php

--- screen-match/index.php
+++ screen-match/index.php
@@ -1,6 +1,8 @@
 <?php
 
+require __DIR__ . "/src/Modelos/Titulo.php";
 require __DIR__ . "/src/Modelos/Genero.php";
+require __DIR__ . "/src/Modelos/Serie.php";
 require __DIR__ . "/src/Modelos/Filme.php";
 
 echo "Bem vindo ao Screen Match";
@@ -9,6 +11,7 @@ $filme = new Filme(
     'Thor: Ragnarok',
     2021,
     Genero::SuperHeroi,
+    180,
 );
 
 $filme->avalia(10);
@@ -20,4 +23,10 @@ var_dump($filme);
 
 echo $filme->media() . "\n";
 
-echo $filme->anoLancamento;
+echo $filme->anoLancamento . "\n";
+
+$serie = new Serie('Lost', 2007, Genero::Drama, 10, 20, 30);
+
+$serie->avalia(8);
+
+echo $serie->media() . "\n";
--- screen-match/src/Modelos/Filme.php
+++ screen-match/src/Modelos/Filme.php
@@ -1,26 +1,14 @@
 <?php
 
-class Filme {
-    private float $media;
-    private array $notas = []; //usado apenas para a própria clss
-
+class Filme extends Titulo {
     public function __construct(
-        public readonly string $nome, 
-        public readonly int $anoLancamento, 
-        public readonly Genero $genero
+        string $nome, 
+        int $anoLancamento, 
+        Genero $genero,
+        public readonly int $duracaoEmMinutos
         ) { //método construct: chamado automaticamente quando um objeto é instanciado (não pode ter um tipo de retorno)
-        $this->notas = []; // qualquer inicialização é colocado no construct
-    } //readonly - são publicas para leituras e privadas para mudança (um só vez)
+            parent::__construct($nome, $anoLancamento, $genero); //permite o acesso a metodos da classe base/mãe
 
-    public function avalia(float $nota): void {
-        $this->notas[] = $nota;
-    }
-
-    public function media(): float {
-        $somaNotas = array_sum($this->notas);
-        $qntdNotas = count($this->notas);
-
-        return $somaNotas / $qntdNotas;
     }
 
     /*
